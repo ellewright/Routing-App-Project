f72b5c89b095000d5346630276b37ad285ab3f44
Eleanor Wright <elliewright055@gmail.com>
2024-09-14 19:03:08 -0500

    Begin work on PostCard component

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom"
+import data from "../../../../api/db.json"
+import { useEffect, useState } from "react"
+
 export default function Posts() {
     return (
         <>
             <h1>Posts</h1>
+            <PostCard />
+        </>
+    )
+}
+
+function PostCard() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        setPosts(data)
+    }, [])
+
+    return (
+        <>
+            {/* posts will be rendered here */}
         </>
     )
 }

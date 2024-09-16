c577af603f9053ac0cbdb4e68728a43868a9f8f1
Eleanor Wright <elliewright055@gmail.com>
2024-09-16 13:00:39 -0500

    Render Todos on Todos page

--- client/src/components/Todos/Todos.jsx
+++ client/src/components/Todos/Todos.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react"
+import api from "../../../../api/db.json"
+import "./Todos.css"
+
 export default function Todos() {
     return (
         <>
             <h1>Todos</h1>
+            <TodoCard />
         </>
+    )
+}
+
+function TodoCard() {
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        setData(api)
+    }, [])
+
+    const todos = data?.todos || []
+    console.log(todos)
+
+    return (
+        <div>
+            {todos.length > 0 ? (
+                todos.map((todo) => (
+                    <div key={todo.id} className={todo.completed === true ? "completed" : ""}>
+                        <h2>{todo.title}</h2>
+                    </div>
+                ))
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
     )
 }
